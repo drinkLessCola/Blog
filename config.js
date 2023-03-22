a14d46416bdab4b0d1b4d78718196e1ede527369
zirina <JayChouZirina@gmail.com>
2023-03-22 11:32:12 +0800

    [fix]: fix api url config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const productionBaseURL = 'https://summerblink.site/blog'
+export const productionBaseURL = 'https://summerblink.site/api'
 export const developmentBaseURL = 'http://localhost:3306'
 // export const baseURL = process.env.NODE_ENV === 'production' ? productionBaseURL : developmentBaseURL
 export const BASE_URL = productionBaseURL
